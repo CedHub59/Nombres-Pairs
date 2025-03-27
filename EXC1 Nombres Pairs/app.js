436fa54f1cc687e08b8969fb05c0ca1f25b1f0c1
CedHub59 <cedric62127@hotmail.fr>
2025-03-27 16:19:04 +0100

    Add files via upload

---
+++ EXC1 Nombres Pairs/app.js
@@ -0,0 +1,15 @@
+// Affiche les nombres de 0 à 20
+for (let i = 0; i <= 20; i++) {
+    if (i === 1) continue; // saute le 1
+    if (i === 3) continue; // saute le 3
+    if (i === 5) continue; // saute le 5
+    if (i === 7) continue; // saute le 7
+    if (i === 9) continue; // saute le 9
+    if (i === 11) continue; // saute le 11
+    if (i === 13) continue; // saute le 13
+    if (i === 15) continue; // saute le 15
+    if (i === 17) continue; // saute le 17
+    if (i === 19) continue; // saute le 19
+    if (i === 20) break; // arrête à 20
+    console.log(i);
+}
